875f97672d5b6ce76250977e72532349c586d723
Giustino De Vincentiis <g.devincentiis@gmail.com>
2018-08-18 18:01:06 +0200

    Update loadcsv.sh
    Added CRL loading

--- loadcsv.sh
+++ loadcsv.sh
@@ -1,5 +1,24 @@
 # accept eventlog in input
 LOG_FILE=$1
+CRL_FILE=$2
+
+if [ ! -n "$2" ]; then
+   CRL_FILE=./cross_reference_list
+fi
+
+declare -A cross_ref_list
+#       -A option declares associative array.
+
+while read -r e i; do 
+	if [[ $e == I_* ]]; 
+	then
+		str=${i//[[:cntrl:]]/}
+		cross_ref_list[$str]=$e
+	fi;
+done < $CRL_FILE
+
+# echo "${!cross_ref_list[*]}"   # The array indices ...
+
 
 if [ ! -n "$1" ]; then
    LOG_FILE=./eventlog
@@ -96,6 +115,8 @@ curl -XPUT 'http://localhost:9200/_ingest/pipeline/parse_logrec_csv?pretty' -H '
  ]
 }'
 
+prefix="IPU object "
+
 # read the CSV file and ingest into Elasticsearch
 while read f1
 do
@@ -108,6 +129,16 @@ do
    # remove control characters
    str3=${str21//[[:cntrl:]]/}
    # echo "$str3"
-   curl -XPOST 'http://localhost:9200/mtlog_v1/logrec?pipeline=parse_logrec_csv&pretty' -H 'Content-Type: application/json' -d" { \"logrec\" : \"$str3\" }"
-   # echo " { \"logrec\" : \"$str3\" } "
+
+   str4=$str3
+   if [[ $str3 =~ $prefix([0-9]+) ]];
+   then
+	echo ${BASH_REMATCH[1]} 
+	# read associative array and replace
+	str4=${str3//$prefix${BASH_REMATCH[1]}/$prefix${cross_ref_list[${BASH_REMATCH[1]}]}}
+        echo "$str4"
+   fi
+
+   curl -XPOST 'http://localhost:9200/mtlog_v1/logrec?pipeline=parse_logrec_csv&pretty' -H 'Content-Type: application/json' -d" { \"logrec\" : \"$str4\" }"
+   # echo " { \"logrec\" : \"$str4\" } "
 done < $FILE_CSV
